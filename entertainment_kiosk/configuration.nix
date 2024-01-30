f9a4c26b5033f96b1ad3414eb5da8711af0f9b47
john-craig <john.e.craig.jr@gmail.com>
2024-01-30 08:55:47 -0500

    Fix error in kiosk, change disk configuration path

--- entertainment_kiosk/configuration.nix
+++ entertainment_kiosk/configuration.nix
@@ -8,11 +8,11 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./disk-config.nix
+      ./disk-configuration.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "entertainment-kiosk"; # Define your hostname.
