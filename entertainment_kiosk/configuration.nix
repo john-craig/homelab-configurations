c3af85f8c1fbfcbb0d0ea2279f3dc8a7193b6671
john-craig <john.e.craig.jr@gmail.com>
2024-02-06 09:54:08 -0500

    Add Ansible dependencies to entertainment_kiosk

--- entertainment_kiosk/configuration.nix
+++ entertainment_kiosk/configuration.nix
@@ -101,6 +101,11 @@
 
   # Bluetooth headest configuration
   # environment.etc."wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  #   bluez_monitor.properties = {
+  #     ["bluez5.roles"] = "[ handsfree_head_unit ]"
+  #   }
+  # '';
+
   #   # bluez_monitor.enabled = true
 	# 	# bluez_monitor.properties = {
 	# 	# 	# ["bluez5.enable-sbc-xq"] = true,
@@ -158,6 +163,47 @@
     python3
     rsync
     usbutils
+    docker
+    (python3.withPackages(ps: with ps; [
+      requests
+      urllib3
+      websocket-client
+      (pkgs.python3Packages.docker.overrideAttrs(oldAttrs: rec {
+         pname = "docker";
+         version = "6.1.3";
+         src = fetchPypi {
+           inherit pname version;
+           sha256 = "sha256-qm0XgwBFul7wFo1eqjTTe+6xE5SMQTr/4dWZH8EfmiA=";
+         };
+      }))
+      (buildPythonPackage rec {
+        pname = "docker-compose";
+        version = "1.29.2";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-TIzZ0h0jdBJ5PRi9MxEASe6a+Nqz/iwhO70HM5WbCbc=";
+        };
+        doCheck = false;
+        propagatedBuildInputs = [
+          (pkgs.python3Packages.docker.overrideAttrs(oldAttrs: rec {
+            pname = "docker";
+            version = "6.1.3";
+            src = fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-qm0XgwBFul7wFo1eqjTTe+6xE5SMQTr/4dWZH8EfmiA=";
+            };
+          }))
+          pkgs.python3Packages.python-dotenv
+          pkgs.python3Packages.dockerpty
+          pkgs.python3Packages.setuptools
+          pkgs.python3Packages.distro
+          pkgs.python3Packages.pyyaml
+          pkgs.python3Packages.jsonschema
+          pkgs.python3Packages.docopt
+          pkgs.python3Packages.texttable
+        ];
+      })
+    ]))
   ];
 
   # List services that you want to enable:
