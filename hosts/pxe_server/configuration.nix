59dfb704bb636afc998d26049a73986983b6a960
john-craig <john.e.craig.jr@gmail.com>
2024-08-16 21:04:24 -0400

    Improve backup script

--- hosts/pxe_server/configuration.nix
+++ hosts/pxe_server/configuration.nix
@@ -59,12 +59,36 @@
     enable = true;
     script =
       ''
+        # Create a /var/run directory, if it doesn't already exist
+        [[ ! -d /var/run/offline-backup/ ]] && mkdir /var/run/offline-backup/
+
+        # Exit if there is still an ongoing backup
+        [[ -f /var/run/offline-backup/backup.pid ]] && exit 0
+
+        # Set our lock
+        echo $$ > /var/run/offline-backup/backup.pid
+
+        # Read the current backup index, creating it if it doesn't exist
+        [[ ! -f /var/run/offline-backup/backup.idx ]] && echo "0" > /var/run/offline-backup/backup.idx
+
+        # Increment the backup index, wrapping at 4
+        BACKUP_IDX=$(cat /var/run/offline-backup/backup.idx)
+        BACKUP_IDX=$((BACKUP_IDX+1))
+        BACKUP_IDX=$((BACKUP_IDX%4))
+
+        # Start the backup
         ${pkgs.gnutar}/bin/tar -czf - /srv/backup/ | \
         ${pkgs.gnupg}/bin/gpg --encrypt --always-trust --recipient offsite-backup \
             --homedir /sec/gnupg/pxe_server/service/.gnupg | \
         ${pkgs.s3cmd}/bin/s3cmd --config=/sec/s3cmd/pxe_server/service/.s3cfg \
             --multipart-chunk-size-mb=500 \
-            put - s3://chiliahedron-offsite-backups/backup.tar.gz.gpg
+            put - s3://chiliahedron-offsite-backups/backup-$BACKUP_IDX.tar.gz.gpg
+        
+        # Update the index only when the backup completed successfully
+        echo $BACKUP_IDX > /var/run/offline-backup/backup.idx
+
+        # Remove our lock
+        rm /var/run/offline-backup/backup.pid
       '';
     serviceConfig = {
       Type = "oneshot";
