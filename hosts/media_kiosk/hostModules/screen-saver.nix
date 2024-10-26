3ced7c2f986215d9d97b8bdc6cfff7518f35ee4a
john-craig <john.e.craig.jr@gmail.com>
2024-10-26 12:55:27 -0400

    media_kiosk: Add feature to screen saver that stores the URL of a page before cycling webpages

--- hosts/media_kiosk/hostModules/screen-saver.nix
+++ hosts/media_kiosk/hostModules/screen-saver.nix
@@ -16,6 +16,7 @@
     environment.systemPackages = with pkgs; [
       chrome-controller
       xprintidle
+      gnugrep
     ];
 
     systemd.timers."screen-saver" = {
@@ -28,7 +29,7 @@
 
     systemd.services."screen-saver" = {
       enable = true;
-      path = [ pkgs.xprintidle pkgs.chrome-controller pkgs.pipewire ];
+      path = [ pkgs.xprintidle pkgs.chrome-controller pkgs.pipewire pkgs.gnugrep ];
       script =
         let
           urlsStr = lib.strings.concatMapStringsSep " " (x: "\"${x}\"") config.screen-saver.urls;
@@ -38,7 +39,13 @@
           URL_LOOP_ARR=(${urlsStr})
           URL_LOOP_LEN=${urlsLen}
 
-          STATE_FILE=/tmp/$USER-:0-screensaver.txt
+          STATE_DIR=/tmp/$USER-:0-screensaver
+
+          STATUS_FILE=$STATE_DIR/status.txt
+          INDEX_FILE=$STATE_DIR/index.txt
+          URL_FILE=$STATE_DIR/saved_url.txt
+          FULLSCREEN_FILE=$STATE_DIR/fullscreen.txt
+
           MAX_IDLE=600
 
           ####################################################################
@@ -62,6 +69,20 @@
           ####################################################################
           if [ $IDLE_TIME -gt $MAX_IDLE ] && [ "$VIDEO_PLAYING" = "False" ]; then
             echo "Screen saver conditions met"
+
+            mkdir -p $STATE_DIR
+
+            if [ ! -f $STATUS_FILE ]; then
+              echo "Stopped" > $STATUS_FILE
+            fi
+
+            if grep -qE 'Stopped' $STATUS_FILE; then
+              # Save information about browser status
+              chromectrl get-focused-tab > $URL_FILE
+              chromectrl is-video-fullscreen > $FULLSCREEN_FILE
+              echo "Running" > $STATUS_FILE
+            fi
+
             # Exit if we are in full screen
             chromectrl exit-fullscreen
 
@@ -69,10 +90,10 @@
             # Retrieve or initialize URL index
             ##################################################################
             echo "Obtaining index"
-            if [ ! -f $STATE_FILE ]; then
+            if [ ! -f $INDEX_FILE ]; then
               URL_IDX=0
             else
-              URL_IDX=$(cat $STATE_FILE)
+              URL_IDX=$(cat $INDEX_FILE)
               URL_IDX=$((URL_IDX+1))
 
               if [ $URL_IDX -ge $URL_LOOP_LEN ]; then
@@ -88,9 +109,11 @@
             NEXT_URL=''${URL_LOOP_ARR[$URL_IDX]}
             echo "Next URL: $NEXT_URL"
             chromectrl focus-tab "''${NEXT_URL}"
-            echo $URL_IDX > $STATE_FILE
+            echo $URL_IDX > $INDEX_FILE
           else
             echo "Screen saver conditions not met, skipping"
+
+            echo "Stopped" > $STATUS_FILE
           fi
         '';
       serviceConfig = {
