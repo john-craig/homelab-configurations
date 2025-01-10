8fe08458613ce5d8269a3d2496f81995e771473c
john-craig <john.e.craig.jr@gmail.com>
2025-01-10 07:35:33 -0500

    Rough draft of authelia proxy

--- hosts/homeserver1/hostModules/containerProxies.nix
+++ hosts/homeserver1/hostModules/containerProxies.nix
@@ -1,4 +1,73 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, config, ... }: let 
+  autheliaConfig = {
+    server = {
+      host = "0.0.0.0";
+      port = 9091;
+    };
+
+    log.level = "info";
+
+    totp = {
+      issuer = "chiliahedron.wtf";
+      period = 30;
+      skew = 1;
+    };
+
+    authentication_backend = {
+      disable_reset_password = true;
+      refresh_interval: "5m";
+
+      file = {
+        path = "/var/lib/authelia/users_database.yml";
+        password = {
+          algorithm = "argon2id";
+          iterations = 4;
+          key_length = 32;
+          salt_length = 16;
+          memory = 4096;
+          parallelism = 8;
+        };
+      };
+    };
+
+    access_control = {
+      default_policy = "deny";
+      rules = [
+        {
+          domain = [ "authelia.chiliahedron.wtf" ];
+          policy = "bypass";
+        }
+        {
+          domain = [ "chiliahedron.wtf" "*.chiliahedron.wtf" ];
+          policy = "one_factor";
+        }
+      ];
+    };
+
+    session = {
+      name = "authelia_session";
+      expiration = 86400; # 1 day
+      inactivity = 3600;  # 1 hour
+      domain = "chiliahedron.wtf";
+    };
+
+    regulation = {
+      max_retries = 3;
+      find_time = "10m";
+      ban_time = "12h";
+    };
+
+    storage = {
+      local.path = "/var/lib/authelia/db.sqlite";
+    }
+  };
+
+  autheliaSecrets = {
+    jwtSecretFile = ;
+    sessionSecretFile = ;
+    storageEncryptionKeyFile = ;
+  };
+in {
   options = {
     containerProxies = {
       enable = lib.mkEnableOption "configuration for Podman container proxies";
